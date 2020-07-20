fa17512275f8bd2e04abf2998a438f071c29f2d9
Ahmed <ahmadalhaiqi@gmail.com>
2020-07-21 00:20:08 +0800

    Updates

---
+++ precache-manifest.c725155514e6013f0baa3cdc0811fc02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf67f83190fbfc8038f9a79d10d7430",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eeb805b7ec66f4343132",
+    "url": "/monsters-rolodex/static/css/main.72d18085.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb805b7ec66f4343132",
+    "url": "/monsters-rolodex/static/js/main.2f27d0b9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
